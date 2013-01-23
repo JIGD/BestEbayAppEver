a9c886e17c3fdc12b3e693d00fd54488729a07cd
JIGD <igonzalez@nearsoft.com>
2013-01-23 00:55:46 -0700

    integrated functionality

--- BestEbayAppEver/ProductFinder/Logging.py
+++ BestEbayAppEver/ProductFinder/Logging.py
@@ -4,6 +4,6 @@ import xml.dom.minidom
 def xmlResponse(message):
     msg = xml.dom.minidom.parseString(message)
     pretty_xml = msg.toprettyxml()
-    log = open("response.txt", 'w')
+    log = open("response.xml", 'w')
     log.write(pretty_xml)
     log.close()
--- BestEbayAppEver/ProductFinder/views.py
+++ BestEbayAppEver/ProductFinder/views.py
@@ -8,6 +8,7 @@ import DoRequest
 def search(request):
     if request.method == 'POST':  # If the form has been submitted...
         product = str(request.POST['product'])  # look for variable product in POST
+        DoRequest.findByKeyword(product)
         return render_to_response('searchResults.html', {'output': product}) 
     else:
         return render(request, 'home.html')
