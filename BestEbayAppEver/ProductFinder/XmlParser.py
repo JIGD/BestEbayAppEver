01821b28fe7aef0ea6ffc5b644fa2c3e6cc1117c
JIGD <igonzalez@nearsoft.com>
2013-01-23 03:06:35 -0700

    Added support for selling status

--- BestEbayAppEver/ProductFinder/XmlParser.py
+++ BestEbayAppEver/ProductFinder/XmlParser.py
@@ -10,6 +10,12 @@ def processMessage(message):
     for child in items:
         item = {}  # clean the item dictionary
         for children in child:
-            item[children.tag[52:]] = children.text  # remove nasty prefix and store each value with its tag as a key on an item
+            if children.tag[52:] != 'sellingStatus':
+                item[children.tag[52:]] = children.text  # remove nasty prefix and store each value with its tag as a key on an item
+            else:
+                selling_status = {}
+                for status in children:
+                    selling_status[status.tag[52:]] = status.text
+                item[children.tag[52:]] = selling_status
         total_items.append(item)  # inser item into the list
     return total_items
