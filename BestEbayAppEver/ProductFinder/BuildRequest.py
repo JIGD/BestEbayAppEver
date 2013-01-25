90a97ca8a5deae1f6b105aa83cfa7752eb445df6
igonzalez <igonzalez@ezrez.com>
2013-01-25 10:49:57 -0700

    Changed default elements in request

--- BestEbayAppEver/ProductFinder/BuildRequest.py
+++ BestEbayAppEver/ProductFinder/BuildRequest.py
@@ -26,7 +26,7 @@ def buildRequestXml(keyword):
               "xmlns=\"http://www.ebay.com/marketplace/search/v1/services\">" +\
               "<keywords>" + keyword + "</keywords>" + \
               "<paginationInput>" + \
-              "<entriesPerPage>5</entriesPerPage>" + \
+              "<entriesPerPage>15</entriesPerPage>" + \
               "</paginationInput>" + \
               "</findItemsByKeywordsRequest>"
     return requestXml
